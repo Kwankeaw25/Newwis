0e7e1fe3fc0f86a21526974c6ef28f21eebbf394
Chrxnxs <nave070552@gmail.com>
2026-03-27 22:20:05 +0700

    test-commit

--- Api/Agent1.py
+++ Api/Agent1.py
@@ -4,7 +4,6 @@ import asyncio
 import os
 from dotenv import load_dotenv
 
-
 # Load environment variables from .env file
 load_dotenv()
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "").strip('"').strip("'").strip()
